5bed27274aff77a64e2d232df23e5bf3fcadd835
Saquib Nasar Siddiqui <saquibsiddiqui023@gmail.com>
2025-03-02 14:59:46 +0530

    refactor Hero component structure and update font-face definitions

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -23,43 +23,59 @@ const Hero = () => {
   const getVideoSrc = (index) => `videos/hero-${index}.mp4`;
 
   return (
-    <div className="relative h-dvh w-screen overflow-x-hidden">
-      <div
-        id="video-frame"
-        className="relative z-10 h-dvh w-screen overflow-hidden rounded-lg bg-blue-75"
-      >
+    <>
+      <div className="relative h-dvh w-screen overflow-x-hidden">
         <div
-          className="mask-clip-path absolute-center absulate z-50 size-64 cursor-pointer 
-          overflow-hidden rounded-lg"
+          id="video-frame"
+          className="relative z-10 h-dvh w-screen overflow-hidden rounded-lg bg-blue-75"
         >
           <div
-            className="orign-center scale-50 opacity-0 transition-all duration-500 ease-in-out hover:scale-100 hover:opacity-100"
-            onClick={handleMiniVdClick}
+            className="mask-clip-path absolute-center absulate z-50 size-64 cursor-pointer 
+          overflow-hidden rounded-lg"
           >
-            <video
-              ref={nextVideoRef}
-              src={getVideoSrc(upcomingVideoIndex)}
-              loop
-              muted
-              id="current-video"
-              className="size-64 orign-center scale-150 object-cover object-center"
-              onLoadedData={handleVideoLoad}
-            />
-          </div>
+            <div
+              className="orign-center scale-50 opacity-0 transition-all duration-500 ease-in-out hover:scale-100 hover:opacity-100"
+              onClick={handleMiniVdClick}
+            >
+              <video
+                ref={nextVideoRef}
+                src={getVideoSrc(upcomingVideoIndex)}
+                loop
+                muted
+                id="current-video"
+                className="size-64 orign-center scale-150 object-cover object-center"
+                onLoadedData={handleVideoLoad}
+              />
+            </div>
 
-          {/* <div>MiniVideoPlayer</div> */}
+            {/* <div>MiniVideoPlayer</div> */}
+          </div>
+          <video
+            ref={nextVideoRef}
+            src={getVideoSrc(currentIndex)}
+            loop
+            muted
+            id="next-video"
+            className="absolute-center invisible absolute z-20 size-64 orign-center object-cover object-center"
+            onLoadedData={handleVideoLoad}
+          />
+          <video
+            ref={nextVideoRef}
+            src={getVideoSrc(currentIndex)}
+            loop
+            muted
+            autoPlay
+            id="next-video"
+            className="solute left-0 top-0 size-full orign-center object-cover object-center"
+            onLoadedData={handleVideoLoad}
+          />
+
+          <h1 className="special-font hero-heading absolute bottom-5 right-5 z-40 text-blue-75 ">
+            G<b>a</b>ming
+          </h1>
         </div>
-        <video
-          ref={nextVideoRef}
-          src={getVideoSrc(currentIndex)}
-          loop
-          muted
-          id="current-video"
-          className="size-64 orign-center scale-150 object-cover object-center"
-          onLoadedData={handleVideoLoad}
-        />
       </div>
-    </div>
+    </>
   );
 };
 
