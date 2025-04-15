3319b6e4aad720947d6574f54a393cd695a999be
Saquib Nasar Siddiqui <saquibsiddiqui023@gmail.com>
2025-04-15 22:54:34 +0530

    fix: correct typos and improve structure in About component

--- src/components/about.jsx
+++ src/components/about.jsx
@@ -9,8 +9,20 @@ const About = () => {
           Disc<b>o</b>ver the the world&apos;s <br /> l<b>a</b>rgest shared
           adventure
         </div>
+
         <div className="about-subtext">
           <p>The Game of games begins-your life, now an epic MMORPG</p>
+
+          <p>Zentry unites every player from countless games and platfroms</p>
+        </div>
+      </div>
+      <div className="h-dvh">
+        <div className="mask-clip">
+          <img
+            className="absolute left-0 top-0 size-full object-cover"
+            src="img/about.webp"
+            alt="background"
+          />
         </div>
       </div>
     </div>
