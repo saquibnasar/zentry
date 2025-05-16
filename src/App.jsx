0d2eb02502fbd3cf5b76a022520e99f15a153f5d
saquibnasar <saquibsiddiqui023@gmail.com>
2025-05-17 04:26:25 +0530

    feat: add Navbar component; integrate react-use for scroll handling and gsap for animations

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import About from "./components/about";
 import Hero from "./components/Hero";
+import Navbar from "./components/Navbar";
 
 const App = () => {
   return (
     <main className="relative w-screen min-h-screen overflow-x-hidden">
+      <Navbar />
       <Hero />
       <About />
     </main>
